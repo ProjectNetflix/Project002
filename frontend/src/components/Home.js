fbf435fa7026e7ccfed9114514c483fa856f1a5c
Maprang6224 <Maprang6224@gmail.com>
2023-02-12 20:22:04 +0700

    code

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar"
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+            <br/>
+            <h1> ยินดีต้อนรับ </h1>
+        </div>
+    )
+}
+export default Home;
