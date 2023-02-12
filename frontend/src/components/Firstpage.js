4e67abc76b7c46f8568947391e2ca9163082f08f
Maprang6224 <Maprang6224@gmail.com>
2023-02-12 20:21:39 +0700

    code

---
+++ frontend/src/components/Firstpage.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar"
+import { AiFillGithub } from "react-icons/ai";
+
+const Firstpage = () => {
+    return (
+        <div>
+            <br/>
+            <h1> ยินดีต้อนรับ </h1>
+
+        </div>
+    )
+}
+export default Firstpage;
